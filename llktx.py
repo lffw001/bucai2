a4a31f579371c33291be46a591124b1f7f9fd5a3
不才 <123969432+241793@users.noreply.github.com>
2023-07-11 22:57:21 +0800

    Update llktx.py

--- llktx.py
+++ llktx.py
@@ -3,13 +3,16 @@ import datetime
 import requests, logging
 import json, sys, time, os
 from notify import send
+'''''
+new Env("乐乐看提现")
+1.3修复闯关任务不跑   #第一次跑视频需要手动进入APP————视频——————随便点个小视频，领取右边提示4个红包的红包
+乐乐看提现（配合蛋姨的本），变量名:lelekck，需要抓包apillk.cengaw.cn/请求头里面的device#Authorization，（Authorization只需要Bearer后面的部分）
+6.22新增看资讯任务和闯关,多号换行隔开,ua换成自己的（User-Agent）
+需要提5块设一次定时弄在任务本前面
+一天运行2-3次
+cron 0 0,7,15 * * *
+'''''
 
-# 1.3修复闯关任务不跑   #第一次跑视频需要手动进入APP————视频——————随便点个小视频，领取右边提示4个红包的红包
-# 乐乐看提现（配合蛋姨的本），变量名:lelekck，需要抓包apillk.cengaw.cn/请求头里面的device#Authorization，（Authorization只需要Bearer后面的部分）
-# 6.22新增看资讯任务和闯关,多号换行隔开,ua换成自己的（User-Agent）
-# 需要提5块设一次定时弄在任务本前面
-# 一天运行2-3次
-# cron 0 0,7,15 * * *
 money = "5"  # 提现金额，默认5
 ua = "Dalvik/2.1.0 (Linux; U; Android 11; V2055B Build/TP1A.2224.014)"
 
